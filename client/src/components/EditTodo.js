05770b3da558ec16f0cd4d08acd068bfe15470fb
unknown <micjanic20@gmail.com>
2021-08-01 23:12:44 -0500

    edit modal

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -3,19 +3,19 @@ import React, { useState } from "react";
 const EditTodo = ({ todo }) => {
   const [description, setDescription] = useState(todo.description);
 
-  const updateDescription = async () => {
+  const updateDescription = async (e) => {
     e.preventDefault();
     try {
       const body = { description };
       const response = await fetch(
-        `http//localhost:5000/todos/${todo.todo_id}`,
+        `http://localhost:5000/todos/${todo.todo_id}`,
         {
           method: "PUT",
           headers: { "Content-type": "application/json" },
           body: JSON.stringify(body),
         }
       );
-      console.log(response);
+      window.location = "/";
     } catch (err) {
       console.error(err.message);
     }
@@ -32,12 +32,21 @@ const EditTodo = ({ todo }) => {
         Edit
       </button>
 
-      <div className="modal" id={`id${todo.todo_id}`}>
+      <div
+        className="modal"
+        id={`id${todo.todo_id}`}
+        onClick={() => setDescription(todo.description)}
+      >
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
               <h4 className="modal-title">Edit Todo</h4>
-              <button type="button" className="close" data-dismiss="modal">
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                onClick={() => setDescription(todo.description)}
+              >
                 &times;
               </button>
             </div>
@@ -64,6 +73,7 @@ const EditTodo = ({ todo }) => {
                 type="button"
                 className="btn btn-danger"
                 data-dismiss="modal"
+                onClick={() => setDescription(todo.description)}
               >
                 Close
               </button>
